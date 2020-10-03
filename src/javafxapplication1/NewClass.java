f093d70d36220e5d07aaead9270303946a93475b
ArmahRockson <72309097+ArmahRockson@users.noreply.github.com>
2020-10-03 09:22:41 -0700

    created new class

---
+++ src/javafxapplication1/NewClass.java
@@ -0,0 +1,18 @@
+
+package javafxapplication1;
+
+/**
+ *
+ * @author GABONNEY
+ */
+public class NewClass {
+    String name,age,date;
+
+    public NewClass(String name, String age, String date) {
+        this.name = name;
+        this.age = age;
+        this.date = date;
+    }
+    
+    
+}
